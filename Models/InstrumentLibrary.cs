457c60100468fe81ef897fe278fd3235f8c73454
kyllian18 <64402572+kyllian18@users.noreply.github.com>
2021-11-10 16:43:32 +0000

    Initial commit

---
+++ Models/InstrumentLibrary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace InstrumentLibrary.Models
+{
+    public abstract class Instrument
+    {
+        public string SerialNumber { get; set; }
+        public string Name { get; set; }
+
+        public Instrument(string _name)
+        {
+            Name = _name;
+        }
+
+        public abstract void Play();
+    }
+
+    public class Guitar : Instrument
+    {
+        public Guitar(string _name) : base(_name)
+        { }
+
+        public override void Play()
+        {
+            Console.WriteLine($"I am playing {Name} Guitar");
+        }
+    }
+
+    public class Mandolin : Instrument
+    {
+        public Mandolin(string _name) : base(_name)
+        { }
+
+        public override void Play()
+        {
+            Console.WriteLine($"I am playing {Name} Mandolin");
+        }
+    }
+
+    public class Ukulele : Instrument
+    {
+        public Ukulele(string _name) : base(_name)
+        { }
+
+        public override void Play()
+        {
+            Console.WriteLine($"I am playing {Name} Ukulele");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InstrumentLibrary.Models;
+
+namespace InstrumentLibrary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Guitar myGuitar = new Guitar("Gibson");
+            Mandolin myMandolin = new Mandolin("Kentucky KM-140");
+            Ukulele myUkulele = new Ukulele("Ortega");
+
+            myGuitar.Play();
+            myMandolin.Play();
+            myUkulele.Play();
+        }
+    }
+}
